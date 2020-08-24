e7609f7861b494e3c680ac0821343e4130d49c44
Haris Nazir <haris.nazir1963@gmail.com>
2020-08-24 13:47:42 +0100

    Implemented Score and auto generated piped
    Collisions are still not implemeted however we will implement NEAT API first. Also movement is commented out due to neededing to see how pipes are generated. Animation is working well and pipe is set to 600 to ensure higher level of difficulty

--- flappy_bird.py
+++ flappy_bird.py
@@ -4,6 +4,8 @@ import time
 import os
 import random
 
+pygame.font.init()
+
 #Setting window dimensions:
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
@@ -13,6 +15,8 @@ BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bi
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+
+STAT_FONT = pygame.font.SysFont("comicsans", 50)
 
 class Bird:
     IMGS = BIRD_IMGS
@@ -148,10 +152,14 @@ class Base():
         win.blit(self.IMG, (self.x1, self.y))
         win.blit(self.IMG, (self.x2, self.y))
 
-def draw_window(win, bird, pipes, base):
+def draw_window(win, bird, pipes, base, score):
     win.blit(BG_IMG, (0, 0))
     for pipe in pipes:
         pipe.draw(win)
+
+    text = STAT_FONT.render("Score: "+ str(score), 1, (255,255,255))
+    win.blit(text, (WIN_WIDTH - 10 -10 - text.get_width(), 10))
+    
     base.draw(win)
     bird.draw(win)
     pygame.display.update()
@@ -159,9 +167,11 @@ def draw_window(win, bird, pipes, base):
 def main():
     bird = Bird(230,250)
     base = Base(730)
-    pipes = [Pipe(700)]
+    pipes = [Pipe(600)]
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     clock = pygame.time.Clock()
+
+    score = 0
 
     run = True
     while run:
@@ -170,8 +180,34 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        bird.move()
-        draw_window(win, bird, pipes, base)
+        #bird.move()
+        add_pipe = False
+        remove = []
+        for pipe in pipes:
+            if pipe.collide(bird, True):
+                win
+            
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                remove.append(pipe)
+            
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
+
+            pipe.move()
+       
+        if add_pipe:
+            score += 1
+            pipes.append(Pipe(600))
+        
+        for r in remove:
+            pipes.remove(r)
+
+        if bird.y + bird.img.get_height() > 730:
+            pass
+
+        base.move()
+        draw_window(win, bird, pipes, base, score)
 
     pygame.quit()
     quit()
